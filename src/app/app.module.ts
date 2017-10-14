018a2e83d5041770258dd87b8edd17391e277726
Dev Kumar <y2ksoft@gmail.com>
2017-10-14 23:02:16 +1300

    new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import {FormsModule} from '@angular/forms';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddUserComponent
+    AddUserComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,36 @@
+
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  server;
+
+
+  @ViewChild('f') serverForm: NgForm;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+  // onSubmit(formData: NgForm) {
+  //   console.log(this.serverForm.value.content);
+
+  // }
+
+  onAddServer() {
+    console.log(this.serverForm.value.content);
+    this.serverElement.push({
+      name: this.serverForm.value.server,
+      content: this.serverForm.value.content
+    });
+  }
+
+}
